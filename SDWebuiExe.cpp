816a7b4070bff5db6100ce4704625e628cac97a6
kizuk <kizuk@KIZUKU>
2024-03-15 23:14:45 +0900

    プロジェクト ファイルを追加します。

---
+++ SDWebuiExe.cpp
@@ -0,0 +1,23 @@
+﻿#include <cstdlib> // for system function
+#include <iostream>
+#include <string>
+
+int main() {
+    // バッチファイルのパスを指定
+    std::string batchFilePath = "webui-user.bat";
+    std::string UpdateCommand = "git pull";
+
+    // アップデートを実行
+    int result = system(UpdateCommand.c_str());
+
+    if (result == 0) {
+        std::cout << "アップデート( git pull )が正常に実行されました。" << std::endl;
+    }
+    else {
+        std::cout << "アップデート( git pull )の実行中にエラーが発生しました。エラーコード: " << result << std::endl;
+    }
+
+    // バッチファイルを実行
+    int result2 = system(batchFilePath.c_str());
+    return 0;
+}
